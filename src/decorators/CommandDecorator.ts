92635c1880452c0a8c7e0f2e0b0e41ee513db26d
summetdev <summet.dev@gmail.com>
2020-11-15 22:41:16 +0300

    feat: allow decorator chain

--- src/decorators/CommandDecorator.ts
+++ src/decorators/CommandDecorator.ts
@@ -3,17 +3,21 @@ import { Controller, Initializable } from '../bot/core';
 import { CommandExistsError } from '../bot/errors';
 
 export function commandDecorator(options?: CommandOptions): MethodDecorator {
-    return (target: Controller, key: string, descriptor: PropertyDescriptor) => {
-        Initializable.checkInitialization(target);
+    return (target: unknown, key: string, descriptor: PropertyDescriptor) => {
+        if (Controller.isController(target)) {
+            Initializable.checkInitialization(target);
 
-        const command = Command.create(options);
-        command.name ||= key;
-        command.executor ||= descriptor.value;
+            const command = Command.create(options);
+            command.name ||= key;
+            command.executor ||= descriptor.value;
 
-        if (target.commands.contains(command.name)) {
-            throw new CommandExistsError(key);
+            if (target.commands.contains(command.name)) {
+                throw new CommandExistsError(key);
+            }
+
+            target.commands.add(command);
         }
 
-        target.commands.add(command);
+        return target;
     };
 }
--- src/decorators/EventDecorator.ts
+++ src/decorators/EventDecorator.ts
@@ -2,8 +2,12 @@ import { ClientEvents } from 'discord.js';
 import { Controller, Initializable } from '../bot/core';
 
 export function eventDecorator(event?: keyof ClientEvents): MethodDecorator {
-    return (target: Controller, key: string, descriptor: PropertyDescriptor) => {
-        Initializable.checkInitialization(target);
-        target.client.on(event || key, descriptor.value);
+    return (target: unknown, key: string, descriptor: PropertyDescriptor) => {
+        if (Controller.isController(target)) {
+            Initializable.checkInitialization(target);
+            target.client.on(event || key, descriptor.value);
+        }
+
+        return target;
     };
 }
